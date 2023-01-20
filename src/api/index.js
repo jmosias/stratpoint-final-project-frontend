4d5b3dd5560b6d6d8e808f476b2b157918b5ebfe
John Mark Osias <johnmark.osias@stratpoint.com>
2023-01-20 12:53:26 +0800

    Added env

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:7000";
+export const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
 const $http = axios.create({
   baseURL: BASE_URL,
